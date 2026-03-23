2d5157ccbef2fc38022f6832178cc684a78ecc3d
Marian <mmmartinez46@ucatolica.edu.co>
2026-03-23 17:34:37 -0500

    configuraciones iniciales backend y dependencias

--- backend/src/modules/usuario/usuario.routes.ts
+++ backend/src/modules/usuario/usuario.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { authMiddleware } from '../../middleware/auth.middleware'
 import * as controller from './usuario.controller'
 
+
 const router = Router()
 
 router.post('/register', controller.registerUser)
